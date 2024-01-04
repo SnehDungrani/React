75b047c6066d9bdc6a56250b82ef91f9dd297dad
sneh <snehdungrani000@gmail.com>
2024-01-04 18:11:58 +0530

    Add: usestate for filter year

--- react-complete-guide-code-03-react-basics-working-with-components/code/01-starting-setup/src/components/Expenses/Expenses.js
+++ react-complete-guide-code-03-react-basics-working-with-components/code/01-starting-setup/src/components/Expenses/Expenses.js
@@ -1,16 +1,27 @@
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFiler";
 import Card from "../UI/Card";
 import "./Expenses.css";
 
 const Expanses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2024");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
   return (
     <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
         date={props.items[0].date}
-      /> 
-      <ExpenseItem
+      />
+      <ExpenseItem  
         title={props.items[1].title}
         amount={props.items[1].amount}
         date={props.items[1].date}
@@ -27,6 +38,6 @@ const Expanses = (props) => {
       />
     </Card>
   );
-}
+};
 
 export default Expanses;
