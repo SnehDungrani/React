9e620c7dea25639b0b090a0e7cc21a6862f3e2c9
sneh <snehdungrani000@gmail.com>
2024-01-04 15:20:45 +0530

    Add: use state to update a state of title

--- react-complete-guide-code-03-react-basics-working-with-components/code/01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ react-complete-guide-code-03-react-basics-working-with-components/code/01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,25 @@
+import React, { useState } from "react";
 import ExpanseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle('updated')
+    console.log(title);
+  };
   return (
     <Card className="expense-item">
       <ExpanseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">{props.amount} ₹</div>
+        <button onClick={clickHandler}>Change Title</button>
       </div>
     </Card>
-  );
+  ); 
 };
 
 export default ExpenseItem;
