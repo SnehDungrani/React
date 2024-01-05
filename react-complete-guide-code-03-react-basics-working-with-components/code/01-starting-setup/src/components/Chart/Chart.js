df3a1d0715a7da0b27d6cfc89c72d3ecaab23fbd
sneh <snehdungrani000@gmail.com>
2024-01-05 13:18:33 +0530

    change: name of maximum

--- react-complete-guide-code-03-react-basics-working-with-components/code/01-starting-setup/src/components/Chart/Chart.js
+++ react-complete-guide-code-03-react-basics-working-with-components/code/01-starting-setup/src/components/Chart/Chart.js
@@ -3,14 +3,14 @@ import "./Chart.css";
 
 const Chart = (props) => {
   const dataPointValues = props.dataPoints.map((dataPoint) => dataPoint.value);
-  const totalMaxium = Math.max(...dataPointValues);
+  const totalMaximum = Math.max(...dataPointValues);
   return (
     <div className="chart">
       {props.dataPoints.map((dataPoint) => (
         <ChartBar
           key={dataPoint.label}
           value={dataPoint.value}
-          maxValue={totalMaxium}
+          maxValue={totalMaximum}
           label={dataPoint.label}
         />
       ))}
