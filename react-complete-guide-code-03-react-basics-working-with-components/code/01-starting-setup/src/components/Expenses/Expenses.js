5a4dcccfa51cdb3b04328aa1d9d581d525de0bd1
sneh <snehdungrani000@gmail.com>
2024-01-05 13:40:02 +0530

    Change: Default Year

--- react-complete-guide-code-03-react-basics-working-with-components/code/01-starting-setup/src/components/Expenses/Expenses.js
+++ react-complete-guide-code-03-react-basics-working-with-components/code/01-starting-setup/src/components/Expenses/Expenses.js
@@ -6,7 +6,7 @@ import ExpensesList from "./ExpensesList";
 import ExpensesChart from "./ExpensesChart";
 
 const Expanses = (props) => {
-  const [filteredYear, setFilteredYear] = useState("2022");
+  const [filteredYear, setFilteredYear] = useState("2021");
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
