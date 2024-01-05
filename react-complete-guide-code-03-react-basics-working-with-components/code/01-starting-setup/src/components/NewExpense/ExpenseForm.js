d02eddb81de1c699979390cf02e2cae0a79c7efd
sneh <snehdungrani000@gmail.com>
2024-01-05 15:24:57 +0530

    Add: button for cancel

--- react-complete-guide-code-03-react-basics-working-with-components/code/01-starting-setup/src/components/NewExpense/ExpenseForm.js
+++ react-complete-guide-code-03-react-basics-working-with-components/code/01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -81,13 +81,16 @@ const ExpenseForm = (props) => {
           <input
             type="Date"
             min="2023-01-01"
-            max="2024-01-04"
+            max="2024-01-05"
             value={enteredDate}
             onChange={dateChangeHandler}
           />
         </div>
       </div>
       <div className="new-expense__actions">
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
