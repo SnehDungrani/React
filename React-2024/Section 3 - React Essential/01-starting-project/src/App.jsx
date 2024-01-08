4c87f44396a2ca957b80c83d19ed96966926cecc
sneh <snehdungrani000@gmail.com>
2024-01-08 16:28:28 +0530

    Refactor code

--- React-2024/Section 3 - React Essential/01-starting-project/src/App.jsx
+++ React-2024/Section 3 - React Essential/01-starting-project/src/App.jsx
@@ -1,36 +1,6 @@
-import reactImg from "./assets/react-core-concepts.png";
-import componentImg from "./assets/Components.png";
+import Header from "./Components/Header/Header";
+import CoreConcept from "./Components/CoreConcept";
 import { CORE_CONCEPTS } from "./Data";
-
-const reactDescriptions = ["Fundamental", "Crucial", "Core"];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function CoreConcept({ image, title, description }) {
-  return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  );
-}
-
-function Header() {
-  const description = reactDescriptions[genRandomInt(2)];
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
 
 function App() {
   return (
