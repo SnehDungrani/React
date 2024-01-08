61e2a18712c89f59b7afd0991f2a5fc6b45d508c
sneh <snehdungrani000@gmail.com>
2024-01-08 11:18:05 +0530

    Add: error message

--- 03-Practice-project/src/Component/Users/AddUser.js
+++ 03-Practice-project/src/Component/Users/AddUser.js
@@ -7,14 +7,24 @@ import ErrorModel from "../UI/ErrorModel";
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
+  const [error, setError] = useState();
 
   const addUserhandler = (e) => {
     e.preventDefault();
 
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      setError({
+        title: "Invalid Input",
+        message: "Please enter an valid name and age(non-empty value) ",
+      });
+
       return;
     }
     if (+enteredAge < 1) {
+      setError({
+        title: "Invalid Age",
+        message: "Please enter a valid age (> 0) ",
+      });
       return;
     }
     props.onAddUser(enteredUsername, enteredAge);
@@ -30,9 +40,19 @@ const AddUser = (props) => {
     setEnteredAge(e.target.value);
   };
 
+  const errorHandler = () => {
+    setError(null);
+  };
+
   return (
     <div>
-      <ErrorModel title="An error occurred!" message="something went wrong" />
+      {error && (
+        <ErrorModel
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
       <Card className={classes.input}>
         <form onSubmit={addUserhandler}>
           <label htmlFor="username">Username</label>
