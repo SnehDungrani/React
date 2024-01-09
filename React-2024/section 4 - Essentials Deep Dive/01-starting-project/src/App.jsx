7958860fc0c15432c331166f383c278d0f0fbfee
sneh <snehdungrani000@gmail.com>
2024-01-09 16:17:51 +0530

    Remove: Extra Import

--- React-2024/section 4 - Essentials Deep Dive/01-starting-project/src/App.jsx
+++ React-2024/section 4 - Essentials Deep Dive/01-starting-project/src/App.jsx
@@ -1,6 +1,4 @@
-import { useState } from "react";
 import Header from "./components/Header/Header.jsx";
-import { EXAMPLES } from "./data.js";
 import Examples from "./components/Examples.jsx";
 import CoreConcepts from "./components/CoreConcepts.jsx";
 
