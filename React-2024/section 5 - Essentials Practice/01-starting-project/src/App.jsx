b7ee21b812c1abdae23670b2798b75d63e3be7ae
sneh <snehdungrani000@gmail.com>
2024-01-10 18:26:19 +0530

    Final Commit

--- React-2024/section 5 - Essentials Practice/01-starting-project/src/App.jsx
+++ React-2024/section 5 - Essentials Practice/01-starting-project/src/App.jsx
@@ -1,7 +1,31 @@
+import Results from "./Components/Results";
+import UserInput from "./Components/UserInput";
+import { useState } from "react";
+
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  const inputIsValid = userInput.duration >= 1;
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return { ...prevUserInput, [inputIdentifier]: +newValue };
+    });
+  }
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <>
+      <UserInput userInput={userInput} onChange={handleChange} />
+      {!inputIsValid && (
+        <p className="center">Please enter a duration grater than 0</p>
+      )}
+      {inputIsValid && <Results input={userInput} />}
+    </>
+  );
 }
 
-export default App
+export default App;
