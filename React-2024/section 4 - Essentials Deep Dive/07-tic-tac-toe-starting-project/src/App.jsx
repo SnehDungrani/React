fcc1c2984d6b272a0c64deaf7786bda57b8536ba
sneh <snehdungrani000@gmail.com>
2024-01-10 14:11:31 +0530

    Add: state for update turn

--- React-2024/section 4 - Essentials Deep Dive/07-tic-tac-toe-starting-project/src/App.jsx
+++ React-2024/section 4 - Essentials Deep Dive/07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,16 +1,49 @@
+import { useState } from "react";
 import Player from "./Components/Player";
 import GameBoard from "./Components/GameBoard";
+import Log from "./Components/Log";
 
 function App() {
+  const [gameTurn, setGameTurn] = useState([]);
+  const [activePlayer, setActivePlayer] = useState("X");
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    setActivePlayer((currActivePlayer) =>
+      currActivePlayer === "X" ? "O" : "X"
+    );
+
+    setGameTurn((prevTurn) => {
+      let currentPlayer = "X";
+      console.log(prevTurn);
+      if (prevTurn.length > 0 && prevTurn[0].Player === "X") {
+        currentPlayer = "O";
+      }
+      const updatedTurn = [
+        { square: { row: rowIndex, col: colIndex }, Player: activePlayer },
+        ...prevTurn,
+      ];
+      return updatedTurn;
+    });
+  }
+
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player initialName="Player 1" symbol="X" />
-          <Player initialName="Player 2" symbol="O" />
+        <ol id="players" className="highlight-player">
+          <Player
+            initialName="Player 1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+          />
+          <Player
+            initialName="Player 2"
+            symbol="O"
+            isActive={activePlayer === "O"}
+          />
         </ol>
-        <GameBoard />
+        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurn} />
       </div>
+      <Log turns={gameTurn} />
     </main>
   );
 }
