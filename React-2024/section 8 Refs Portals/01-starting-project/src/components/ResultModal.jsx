81f393a91afb1d599752042aebd67a28bd9c7973
sneh <snehdungrani000@gmail.com>
2024-01-12 15:38:17 +0530

    Add: score and time

--- React-2024/section 8 Refs Portals/01-starting-project/src/components/ResultModal.jsx
+++ React-2024/section 8 Refs Portals/01-starting-project/src/components/ResultModal.jsx
@@ -1,10 +1,14 @@
 import { useImperativeHandle, forwardRef, useRef } from "react";
 
 const ResultModal = forwardRef(function ResultModal(
-  { results, targetTime },
+  { targetTime, remainingTime, onReset },
   ref
 ) {
   const dialog = useRef();
+
+  const userLost = remainingTime <= 0;
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+  const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
 
   useImperativeHandle(ref, () => {
     return {
@@ -14,15 +18,17 @@ const ResultModal = forwardRef(function ResultModal(
     };
   });
   return (
-    <dialog ref={dialog} className="result-modal">
-      <h2>Your {results}</h2>
+    <dialog ref={dialog} className="result-modal" onClose={onReset}>
+      {userLost && <h2>Your lost</h2>}
+      {!userLost && <h2>Your score: {score}</h2>}
       <p>
         The target time was <strong>{targetTime} seconds.</strong>
       </p>
       <p>
-        You stop the time <strong>X seconds left.</strong>
+        You stop the time.
+        <strong>{formattedRemainingTime} seconds left.</strong>
       </p>
-      <form method="dialog">
+      <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
     </dialog>
