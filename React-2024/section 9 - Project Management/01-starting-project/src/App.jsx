aff4e579b9a501578e9391cb10e8a8d9d057f181
sneh <snehdungrani000@gmail.com>
2024-01-15 12:06:31 +0530

    Manage: projects state

--- React-2024/section 9 - Project Management/01-starting-project/src/App.jsx
+++ React-2024/section 9 - Project Management/01-starting-project/src/App.jsx
@@ -1,9 +1,47 @@
+import { useState } from "react";
+import NewProject from "./Components/NewProject";
+import NoProjectSelected from "./Components/NoProjectSelected";
 import ProjectSideBar from "./Components/ProjectSideBar";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  const handleStartAddProject = () => {
+    setProjectsState((prevState) => {
+      return { ...prevState, selectedProjectId: null };
+    });
+  };
+
+  const handleAddProject = (projectData) => {
+    setProjectsState((prevState) => {
+      const newProjects = {
+        ...projectData,
+        id: Math.random(),
+      };
+
+      return {
+        ...prevState,
+        projects: [...prevState.projects, newProjects],
+      };
+    });
+  };
+  console.log(projectsState);
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject onAdd={handleAddProject} />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSideBar />
+      <ProjectSideBar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
