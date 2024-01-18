63e6a018a7ee75afe01d7a20769028e1be621f3f
sneh <snehdungrani000@gmail.com>
2024-01-18 15:05:31 +0530

    Add: onShowCart props

--- React-2024/section 12 - Food Order App/01-starting-project/src/components/Layout/Header.jsx
+++ React-2024/section 12 - Food Order App/01-starting-project/src/components/Layout/Header.jsx
@@ -1,18 +1,19 @@
 import React from "react";
+import { Fragment } from "react";
 import classes from "./Header.module.css";
 import mealsImage from "../../assets/meals.jpg";
 import HeaderCartButton from "./HeaderCartButton";
 
-export default function Header(props) {
+export default function Header({ onShowCart }) {
   return (
-    <>
+    <Fragment>
       <header className={classes.header}>
         <h1>Food App</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onShowCart={onShowCart} />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="Meal Img" />
       </div>
-    </>
+    </Fragment>
   );
 }
