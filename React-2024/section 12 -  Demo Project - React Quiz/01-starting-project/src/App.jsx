c67e6a93bf4db1256f9d3daf000e067fd9bf7909
sneh <snehdungrani000@gmail.com>
2024-01-18 10:18:42 +0530

    Final commit

--- React-2024/section 12 -  Demo Project - React Quiz/01-starting-project/src/App.jsx
+++ React-2024/section 12 -  Demo Project - React Quiz/01-starting-project/src/App.jsx
@@ -1,3 +1,17 @@
-function App() {}
+import React from "react";
+
+import Header from "./components/Header";
+import Quiz from "./components/Quiz";
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+      <main>
+        <Quiz />
+      </main>
+    </React.Fragment>
+  );
+}
 
 export default App;
