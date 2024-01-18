117cfc6ae1914f81bc76eb466ecb984f82781928
sneh <snehdungrani000@gmail.com>
2024-01-18 11:15:11 +0530

    Add: Meals in App component

--- React-2024/section 12 - Food Order App/01-starting-project/src/App.js
+++ React-2024/section 12 - Food Order App/01-starting-project/src/App.js
@@ -1,7 +1,17 @@
+import { Fragment } from "react";
 import Header from "./components/Layout/Header";
+import Meals from "./components/Meal/Meals";
+import Card from "./components/UI/Card";
 
 function App() {
-  return <Header />;
+  return (
+    <Fragment>
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </Fragment>
+  );
 }
 
 export default App;
