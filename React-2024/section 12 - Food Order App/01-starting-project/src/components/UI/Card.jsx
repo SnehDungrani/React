39eb24ac701b3f3656b905c32c1c7747c91158a6
sneh <snehdungrani000@gmail.com>
2024-01-18 12:06:45 +0530

    Change: props to destructure

--- React-2024/section 12 - Food Order App/01-starting-project/src/components/UI/Card.jsx
+++ React-2024/section 12 - Food Order App/01-starting-project/src/components/UI/Card.jsx
@@ -1,6 +1,5 @@
-import Meals from "../Meal/Meals";
 import classes from "./Card.module.css";
 
-export default function Card(props) {
-  return <div className={classes.card}>{props.children}</div>;
+export default function Card({ children }) {
+  return <div className={classes.card}>{children}</div>;
 }
