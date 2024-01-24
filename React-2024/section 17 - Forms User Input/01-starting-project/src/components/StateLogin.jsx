f70329b1a4dfd343862530367426765cdd8d53b0
sneh <snehdungrani000@gmail.com>
2024-01-24 14:52:49 +0530

    Add: Email validation

---
+++ React-2024/section 17 - Forms User Input/01-starting-project/src/components/StateLogin.jsx
@@ -0,0 +1,84 @@
+import { useState } from "react";
+
+export default function Login() {
+  const [enteredValue, setEnteredValue] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [didEdit, setDidEdit] = useState({
+    email: false,
+    password: false,
+  });
+
+  const emailIsInvalid = didEdit.email && !enteredValue.email.includes("@");
+
+  function handleInputChange(identifier, value) {
+    setEnteredValue((prevValue) => ({
+      ...prevValue,
+      [identifier]: value,
+    }));
+
+    setDidEdit((prevEdit) => ({
+      ...prevEdit,
+      [identifier]: false,
+    }));
+  }
+
+  function handleInputBlur(initialValue) {
+    setDidEdit((prevEdit) => ({
+      ...prevEdit,
+      [initialValue]: true,
+    }));
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Login</h2>
+
+      <div className="control-row">
+        <div className="control no-margin">
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onBlur={() => {
+              handleInputBlur("email");
+            }}
+            onChange={(e) => handleInputChange("email", e.target.value)}
+            value={enteredValue.email}
+          />
+          <div className="control-error">
+            {emailIsInvalid && <p>Please enter a valid email.</p>}
+          </div>
+        </div>
+
+        <div className="control no-margin">
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onBlur={() => {
+              handleInputBlur("password");
+            }}
+            onChange={(e) => handleInputChange("password", e.target.value)}
+            value={enteredValue.password}
+          />
+        </div>
+      </div>
+
+      <p className="form-actions">
+        <button className="button button-flat">Reset</button>
+        <button type="submit" className="button">
+          Login
+        </button>
+      </p>
+    </form>
+  );
+}
