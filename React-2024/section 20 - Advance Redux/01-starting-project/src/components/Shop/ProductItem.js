fa3be4d024bc7e4ab30a8cf1bb0048200b2fa043
sneh <snehdungrani000@gmail.com>
2024-01-30 15:47:42 +0530

    Final Commit

--- React-2024/section 20 - Advance Redux/01-starting-project/src/components/Shop/ProductItem.js
+++ React-2024/section 20 - Advance Redux/01-starting-project/src/components/Shop/ProductItem.js
@@ -1,16 +1,26 @@
 import { useDispatch } from "react-redux";
 
-import { cartActions } from "../../store/cart-slice.js";
+import { cartActions } from "../../store/cart-slice";
 import Card from "../UI/Card";
 import classes from "./ProductItem.module.css";
 
 const ProductItem = (props) => {
   const dispatch = useDispatch();
+
   const { title, price, description, id } = props;
 
-  const addCartHandler = () => {
-    dispatch(cartActions.addItemToCart({ id, title, price }));
+  const addToCartHandler = () => {
+    // and then send Http request
+    // fetch('firebase-url', { method: 'POST', body: JSON.stringify(newCart) })
+    dispatch(
+      cartActions.addItemToCart({
+        id,
+        title,
+        price,
+      })
+    );
   };
+
   return (
     <li className={classes.item}>
       <Card>
@@ -20,7 +30,7 @@ const ProductItem = (props) => {
         </header>
         <p>{description}</p>
         <div className={classes.actions}>
-          <button onClick={addCartHandler}>Add to Cart</button>
+          <button onClick={addToCartHandler}>Add to Cart</button>
         </div>
       </Card>
     </li>
