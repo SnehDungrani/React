ccc61ef9a51332f2753d92a546458ffbdbb4f842
sneh <snehdungrani000@gmail.com>
2024-02-01 10:41:50 +0530

    Add: root

--- React-2024/section 21 - Routing/01-starting-project/src/App.js
+++ React-2024/section 21 - Routing/01-starting-project/src/App.js
@@ -1,5 +1,39 @@
+import {
+  createBrowserRouter,
+  // createRoutesFromElements,
+  // Route,
+  RouterProvider,
+} from "react-router-dom";
+import HomePage from "./pages/Home";
+import ProductPage from "./pages/Products";
+import RootLayout from "./pages/Root";
+import ErrorPage from "./pages/Error";
+import ProductDetailPage from "./pages/ProductDetail";
+
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage />} />
+//     <Route path="/product" element={<ProductPage />} />,
+//   </Route>
+// );
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { path: "", element: <HomePage /> },
+      { path: "products", element: <ProductPage /> },
+      { path: "products/:productId", element: <ProductDetailPage /> },
+    ],
+  },
+]);
+
+// const router = createBrowserRouter(routeDefinitions);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
