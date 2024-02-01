a3659dd7d0901ee8e6f5b6fa19cf735264611f95
sneh <snehdungrani000@gmail.com>
2024-02-01 17:02:05 +0530

    Created: Loader fn and error

---
+++ React-2024/section 21 - Routing/02-starting-project/frontend/src/pages/Events.js
@@ -0,0 +1,32 @@
+import { useLoaderData } from "react-router-dom";
+import EventsList from "../components/EventsList";
+
+function EventsPage() {
+  const data = useLoaderData();
+
+  // if (data.isError) {
+  //   return <p>{data.message}</p>;
+  // }
+  const events = data.events;
+  return <EventsList events={events} />;
+}
+
+export default EventsPage;
+
+export async function Loader() {
+  const response = await fetch("http://localhost:8080/events");
+
+  if (!response.ok) {
+    // return { isError: true, message: "Could not fetch events." };
+
+    throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
+      status: 500,
+    });
+  } else {
+    // const resData = await response.json();
+    // const res = new Response("any data", { status: 201 });
+    // return resData.events;
+
+    return response;
+  }
+}
