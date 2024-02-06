b9d3dbc50ef60f78c959c74a2269e698530688f1
sneh <snehdungrani000@gmail.com>
2024-02-06 09:25:56 +0530

    First commit

---
+++ React-2024/section 25 - NextJS App Router Introduction/foodies-starting-project/01-starting-project/app/meals/[slug]/page.js
@@ -0,0 +1,3 @@
+export default function MealsItemPage() {
+  return <h1>Meal Item Page</h1>;
+}
---
+++ React-2024/section 25 - NextJS App Router Introduction/foodies-starting-project/01-starting-project/app/meals/page.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+export default function Meals() {
+  return (
+    <main>
+      <h1>The Meals</h1>
+      <p>
+        <Link href="/meals/meal-1">Meal 1</Link>
+      </p>
+      <p>
+        <Link href="/meals/meal-2">Meal 2</Link>
+      </p>
+    </main>
+  );
+}
