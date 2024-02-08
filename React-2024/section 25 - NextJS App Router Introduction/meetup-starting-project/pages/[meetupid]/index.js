9b179a65fe1a06cf084e8ddbb751dfb428dd6983
sneh <snehdungrani000@gmail.com>
2024-02-08 18:15:30 +0530

    First commit

---
+++ React-2024/section 25 - NextJS App Router Introduction/meetup-starting-project/pages/[meetupid]/index.js
@@ -0,0 +1,48 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+export default function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1200px-Stadtbild_M%C3%BCnchen.jpg?20130611211153"
+      title="A First Meetup"
+      address="Some Street 5, Some City"
+      description="The meetup description"
+    />
+  );
+}
+
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1200px-Stadtbild_M%C3%BCnchen.jpg?20130611211153",
+        id: meetupId,
+        title: "A First Meetup",
+        address: "Some Street 5, Some City",
+        description: "The meetup description",
+      },
+    },
+  };
+}
