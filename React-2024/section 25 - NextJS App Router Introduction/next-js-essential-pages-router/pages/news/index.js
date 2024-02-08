6f2d39aafee4f2d524ce80bd0230d3d4ad79203e
sneh <snehdungrani000@gmail.com>
2024-02-08 11:54:03 +0530

    final commit

--- React-2024/section 25 - NextJS App Router Introduction/next-js-essential-pages-router/pages/news/index.js
+++ React-2024/section 25 - NextJS App Router Introduction/next-js-essential-pages-router/pages/news/index.js
@@ -1,3 +1,18 @@
+import Link from "next/link";
+import { Fragment } from "react";
+
 export default function NewsPage() {
-  return <h1>The News Page</h1>;
+  return (
+    <Fragment>
+      <h1>The news page</h1>
+      <ul>
+        <li>
+          <Link href="news/nextJS-is-a-great-framework">
+            NextJS is a great framework.
+          </Link>
+        </li>
+        <li>Something Else</li>
+      </ul>
+    </Fragment>
+  );
 }
