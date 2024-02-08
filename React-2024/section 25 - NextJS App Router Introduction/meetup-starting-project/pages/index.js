d128b770871bcb63d53db05429f6af119b057d9d
sneh <snehdungrani000@gmail.com>
2024-02-08 12:33:54 +0530

    Add: Dummy meetup and pass

---
+++ React-2024/section 25 - NextJS App Router Introduction/meetup-starting-project/pages/index.js
@@ -0,0 +1,24 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A First Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1200px-Stadtbild_M%C3%BCnchen.jpg?20130611211153",
+    address: "Some address 5, 12345 Some City",
+    description: "This is a first meetup!",
+  },
+  {
+    id: "m2",
+    title: "A Second Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1200px-Stadtbild_M%C3%BCnchen.jpg?20130611211153",
+    address: "Some address 10, 12345 Some City",
+    description: "This is a Second meetup!",
+  },
+];
+
+export default function HomePage() {
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
+}
